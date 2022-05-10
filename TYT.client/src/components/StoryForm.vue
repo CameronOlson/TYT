64c8d605b366af922e2d296273bcf52b77612cf0
CameronOlson <olsoncam10@gmail.com>
2022-05-09 21:25:19 -0600

    Working on the recording modal

---
+++ TYT.client/src/components/StoryForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="component">
+    <section class="main-controls">
+      <canvas class="visualizer" height="60px"></canvas>
+      <div id="buttons">
+        <button class="record">Record</button>
+        <button class="stop">Stop</button>
+      </div>
+    </section>
+
+    <section class="sound-clips"></section>
+  </div>
+
+  <label for="toggle">❔</label>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
--- TYT.client/src/pages/HomePage.vue
+++ TYT.client/src/pages/HomePage.vue
@@ -1,36 +1,22 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div>
+    <button data-bs-toggle="modal" data-bs-target="#create-story">
+      Tell A Story
+    </button>
   </div>
+  <Modal id="create-story">
+    <template #modal-title>
+      <h6>Tell Your Tale</h6>
+    </template>
+    <template #modal-body>
+      <StoryForm />
+    </template>
+  </Modal>
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+
 </script>
 
 <style scoped lang="scss">
-.home{
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card{
-    width: 50vw;
-    > img{
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
 </style>
